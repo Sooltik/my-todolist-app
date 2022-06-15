cf040b3d86ac3140c97a720e8ae9d6f10e704fae
Sooltik <niazov.sultan@gmail.com>
2022-06-15 13:51:08 -0400

    feat: add method addTask(); edit: header hide-on-scroll

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,33 @@
 <template>
-  <v-app>
+<v-app>
     <!-- Header -->
-    <v-app-bar
-      app
-      color="primary"
-      dark
-      prominent
-      shrink-on-scroll
-      src="mountains.png"
-      scroll-target="#scrolling-techniques-2"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
+    <v-app-bar app color="primary" dark prominent hide-on-scroll src="mountains.png">
+        <template v-slot:img="{ props }">
+            <v-img v-bind="props" gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"></v-img>
+        </template>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+        <HelloWorld />
     </v-main>
-  </v-app>
+</v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld,
+    },
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,60 @@
 <template>
-  <v-container>
+<v-container>
     <v-row class="text-center">
-      <v-col class="mb-4">
+        <v-col class="mb-4">
 
-        <!-- Title -->
-        <h1 class="display-2 font-weight-bold ma-4">
-          My Todo List
-        </h1>
+            <!-- Title -->
+            <h1 class="display-2 font-weight-bold ma-4">
+                My Todo List
+            </h1>
 
-        <!-- Form Input Text Field -->
-        <v-text-field class="pa-3" outlined label="Add task" append-icon="mdi-plus-box"></v-text-field>
+            <!-- Form Input Text Field -->
+            <v-text-field v-model="taskName" class="pa-3" outlined label="Add task" @click:append="addTask" @keyup.enter="addTask" append-icon="mdi-plus-box">
+            </v-text-field>
 
-        <!-- List of tasks -->
-        <div v-for="task in tasks" :key="task.id">
-          <v-list class="pt-0" flat>
-            <v-list-item>
-              <template v-slot:default="{ active, }">
-                <v-list-item-action>
-                  <v-checkbox :input-value="active" color="primary"></v-checkbox>
-                </v-list-item-action>
+            <!-- List of tasks -->
+            <div v-for="task in tasks" :key="task.id">
+                <v-list class="pt-0" flat>
+                    <v-list-item>
+                        <template v-slot:default="{ active, }">
+                            <v-list-item-action>
+                                <v-checkbox :input-value="active" color="primary"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>{{ task.title }}</v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                    </v-list-item>
+                    <v-divider></v-divider>
+                </v-list>
+            </div>
 
-                <v-list-item-content>
-                  <v-list-item-title>{{ task.title }}</v-list-item-title>
-                </v-list-item-content>
-              </template>
-            </v-list-item>
-          </v-list>
-        </div>
-
-      </v-col>
+        </v-col>
     </v-row>
-  </v-container>
+</v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+    name: 'HelloWorld',
 
-  data() {
-    return {
-      tasks: [
-        {
-          id: 1,
-          title: "Sleep",
-          done: false
-        },
-        {
-          id: 2,
-          title: "Code",
-          done: false
-        },
-        {
-          id: 3,
-          title: "Repeat",
-          done: false
-        },]
+    data() {
+        return {
+            taskName: "",
+            tasks: []
+        }
+    },
+
+    methods: {
+        addTask() {
+            let newTask = {
+                id: Date.now(),
+                title: this.taskName,
+                done: false
+            }
+            this.tasks.push(newTask);
+            this.taskName = "";
+        }
     }
-  },
 }
 </script>
